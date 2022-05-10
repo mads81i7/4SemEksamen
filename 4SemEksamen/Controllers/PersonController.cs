a94842c90de386c8059340c83004de25cae00dc5
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2022-05-10 15:52:48 +0200

    Add project files.

---
+++ 4SemEksamen/Controllers/PersonController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using _4SemEksamen.Models;
+using _4SemEksamen.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _4SemEksamen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PersonsManager Manager;
+
+        public PersonController()
+        {
+            Manager = new PersonsManager();
+        }
+
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return Manager.GetAll();
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return Manager.GetById(id);
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] Person value)
+        {
+            Manager.Add(value);
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Person value)
+        {
+            Manager.Update(id, value);
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Manager.Delete(id);
+        }
+    }
+}
---
+++ 4SemEksamen/Managers/PersonsManager.cs
@@ -0,0 +1,47 @@
+﻿using _4SemEksamen.Models;
+using System.Collections.Generic;
+
+namespace _4SemEksamen.Managers
+{
+    public class PersonsManager
+    {
+        private static int _nextId = 0;
+        private static List<Person> _persons = new List<Person>()
+        {
+            new Person() { Id = _nextId++, Name = "Gertrud", PhoneNumber = "10 20 30 40"},
+            new Person() { Id = _nextId++, Name = "Frederik", PhoneNumber = "50 60 70 80"},
+            new Person() { Id = _nextId++, Name = "Hans", PhoneNumber = "90 10 20 30"}
+        };
+        
+        public List<Person> GetAll()
+        {
+            return _persons;
+        }
+
+        //Tilføj exceptions senere
+        public Person GetById(int id)
+        {
+            Person person = _persons.Find(p => p.Id == id);
+            return person;
+        }
+
+        public void Add(Person person)
+        {
+            person.Id = _nextId++;
+            _persons.Add(person);
+        }
+
+        public void Update(int id, Person updates)
+        {
+            Person person = GetById(id);
+            person.Name = updates.Name;
+            person.PhoneNumber = updates.PhoneNumber;
+        }
+
+        public void Delete(int id)
+        {
+            Person person = GetById(_nextId);
+            _persons.Remove(person);
+        }
+    }
+}
